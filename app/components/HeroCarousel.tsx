326b1d6340570cefe89fd8656fcfbb5f8a640ca8
Kamsi <kamsi_orazulike@icloud.com>
2025-02-19 12:11:18 +0000

    height fix

--- app/components/HeroCarousel.tsx
+++ app/components/HeroCarousel.tsx
@@ -177,7 +177,7 @@ const HeroCarousel = () => {
 
   return (
     <div
-      className="relative h-[80vh] md:h-[95vh] w-full overflow-hidden"
+      className="relative h-[88vh] md:h-[96vh] w-full overflow-hidden"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
